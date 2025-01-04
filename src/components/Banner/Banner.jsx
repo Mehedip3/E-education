8b0108fbf7e4f5f5458e55cacc4bf7a5c2dccfa8
mehedip1 <Promehedi30@gmail.com>
2025-01-04 11:29:21 +0600

    new text add

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import cov4 from '/src/assets/images/cov4.jpg';
+import { motion } from 'framer-motion';
+import { Slideup } from '/src/components/utility/animation.jsx';
 
 const Banner = ({ title, subtitle, link }) => {
     // Use passed props or default values
@@ -13,17 +15,30 @@ const Banner = ({ title, subtitle, link }) => {
 
     return (
         <div className="container mx-auto">
-            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
+            <div className="bg-[#f9f9f9] grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
                 {/* Banner Image */}
                 <div>
-                    <img src={cov4} alt="Banner" />
+                    <motion.img
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+                        src={cov4}
+                        alt="Banner"
+                        className="w-[300px] md:max-w-[400px] xl:max-w-[600px] h-auto object-cover"
+                    />
                 </div>
 
                 {/* Banner Info */}
-                <div className="flex flex-col justify-center text-center md:text-left space-y-4">
-                    <p className="text-2xl lg:text-4xl font-bold capitalize font-playfair">
+                <div className="flex flex-col justify-center text-center md:text-left space-y-4 lg:max-w-[500px]">
+                    <motion.p
+                        variants={Slideup (0.5)}
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        className="text-2xl lg:text-4xl font-bold capitalize font-playfair"
+                    >
                         {BannerData.title}
-                    </p>
+                    </motion.p>
                     <p>{BannerData.subtitle}</p>
                     <div className="flex justify-center md:justify-start">
                         <a href={BannerData.link}>
